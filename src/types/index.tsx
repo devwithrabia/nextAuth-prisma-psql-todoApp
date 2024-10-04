8320c5515185615efaf43b1ed6e8278b91f1dd17
Rabia Inam <devwithrabia@gmail.com>
2024-10-04 15:38:36 +0500

    change in type.tsx

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -16,9 +16,6 @@ export interface buttonProps {
   type?: 'button' | 'submit' | 'reset'
   //reactNode means instead of text user can pass icon whose type will be ReactNode
   text: 'string' | ReactNode
-  onClick?: () => void
-  actionButton?: boolean
-  bgColor?: string
 }
 export interface GetData {
   id: string
