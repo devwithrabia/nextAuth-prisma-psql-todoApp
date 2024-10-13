02588e25043c5580403462dc16ed3f22900705d3
Rabia Inam <devwithrabia@gmail.com>
2024-10-13 21:39:41 +0500

    deleted files

--- src/components/TodoApp/Form.tsx
+++
@@ -1,8 +0,0 @@
-import { formProps } from '@/types'
-import { FC } from 'react'
-
-const Form: FC<formProps> = ({ children, onSubmit }) => {
-  //in server actions all data in form autometic transfer to the functiondefined in action
-  return <form onSubmit={onSubmit}>{children}</form>
-}
-export default Form
--- src/components/TodoApp/Input.tsx
+++
@@ -1,7 +0,0 @@
-import { inputProps } from '@/types'
-import { TextField } from '@mui/material'
-import { FC } from 'react'
-
-export const Input: FC<inputProps> = ({ type, placeholder, value, onChange }) => {
-  return <TextField variant='outlined' type={type} placeholder={placeholder} value={value} onChange={onChange} />
-}
