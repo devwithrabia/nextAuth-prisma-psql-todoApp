f859a35ce1ec0c26ff68a1af0ac82687d5b4f6f7
Rabia Inam <devwithrabia@gmail.com>
2025-02-28 08:59:34 +0500

    text change

--- src/pages/forgot-password/success.tsx
+++ src/pages/forgot-password/success.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 
 const SuccessPage: NextPage = () => {
-  return <h1 style={{ color: 'red' }}>Go To Your Gmail and click on th Link given There</h1>
+  return <h3 style={{ color: 'red' }}>Please Go To Your Gmail and click on th Link given There</h3>
 }
 
 export default SuccessPage
